bb3bf359676ba4facaaa62a258c3c723962243f2
gabriellafurgal <gabriellaela144@gmail.com>
2017-08-29 16:59:23 +0200

    Implemented Index view

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.fi
+@user =  User.create(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Jon", last_name: "Snow")
 
+puts "1 User created"
 100.times do |post|
-	Post.create!(date: Date.today,rationale: "#{post} rationale content")
+	Post.create!(date: Date.today,rationale: "#{post} rationale content", user_id: @user.id)
 end
 
 puts "100 Posts have been created"
