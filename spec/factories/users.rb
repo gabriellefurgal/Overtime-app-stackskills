8ed0f5a8d13d444ede77f48322b38e6873328d8d
gabriellafurgal <gabriellaela144@gmail.com>
2017-08-30 13:52:12 +0200

    Added new post button to nevbar

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -15,7 +15,7 @@ FactoryGirl.define do
   factory :admin_user, class: "AdminUser" do
     first_name 'Admin'
     last_name 'User'
-    email 'admin@user.com'
+   email { generate :email }
     password 'asdfasdf'
     password_confirmation 'asdfasdf'
   end
