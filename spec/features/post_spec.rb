2ffc8becbcd7663cfce35788651143dfc8b06307
gabriellafurgal <gabriellaela144@gmail.com>
2017-08-30 14:40:29 +0200

    delete post option added

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -1,89 +1,97 @@
 require 'rails_helper'
 
 describe 'navigate' do
-	before do
+  before do
+    @user = FactoryGirl.create(:user)
+    login_as(@user, :scope => :user)
+  end
 
-			@user = FactoryGirl.create(:user)
- 			login_as(@user, :scope => :user)
- 		end
-	describe 'index' do 
-		before do
-			visit posts_path	
-		end
-		it 'can be reached successfully' do
-			visit posts_path
-			expect(page.status_code).to eq(200)
-		end
+  describe 'index' do
+    before do
+      visit posts_path
+    end
 
-		it 'has a title of Posts' do
-			visit posts_path
-			expect(page).to have_content(/Posts/)
-		end
-		it 'has a list of posts' do
-			post1= @post = FactoryGirl.build_stubbed(:post)
-			post2= @post = FactoryGirl.build_stubbed(:second_post)
+  	it 'can be reached successfully' do
+  		expect(page.status_code).to eq(200)
+  	end
 
-			visit posts_path	
-			expect(page).to have_content(/Rationale|content/)
-		end
-	end
- 	describe 'new' do
- 		it 'has a link from the homepage' do
- 		visit root_path
+  	it 'has a title of Posts' do
+  		expect(page).to have_content(/Posts/)
+  	end
 
- 		click_link("new_post_from_nav")
- 		expect(page.status_code).to eq(200)
- 		end
- 	end
+    it 'has a list of posts' do
+      post1 = FactoryGirl.build_stubbed(:post)
+      post2 = FactoryGirl.build_stubbed(:second_post)
+      visit posts_path
+      expect(page).to have_content(/Rationale|content/)
+    end
+  end
 
+  describe 'new' do
+    it 'has a link from the homepage' do
+      visit root_path
 
- 	describe 'creation' do
- 		before do 
- 		
- 			visit new_post_path
- 		end
+      click_link("new_post_from_nav")
+      expect(page.status_code).to eq(200)
+    end
+  end
 
- 			it 'has a new form that can be reached' do
- 					expect(page.status_code).to eq(200)
- 			end
+  describe 'delete' do
+    it 'can be deleted' do
+      @post = FactoryGirl.create(:post)
+      visit posts_path
 
- 			it 'can be created from new form page' do
- 				fill_in 'post[date]', with: Date.today
-    			fill_in 'post[rationale]', with: "Some rationale"
-      			click_on "Save"
+      click_link("delete_post_#{@post.id}_from_index")
+      expect(page.status_code).to eq(200)
+    end
+  end
 
-     			 expect(page).to have_content("Some rationale")
- 			end
+  describe 'creation' do
+  	before do
+  		visit new_post_path
+  	end
 
- 			it 'will have a user associated it' do
-				fill_in 'post[date]', with: Date.today
-    			fill_in 'post[rationale]', with: "User Association"
-      			click_on "Save"
+  	it 'has a new form that can be reached' do
+  		expect(page.status_code).to eq(200)
+  	end
 
-      			expect(User.last.posts.last.rationale).to eq("User Association")
- 			end
- 	end
+  	it 'can be created from new form page' do
+      fill_in 'post[date]', with: Date.today
+      fill_in 'post[rationale]', with: "Some rationale"
+      click_on "Save"
 
- 	describe 'edit' do
- 		before do 
- 			@post= FactoryGirl.create(:post)
- 		end
- 		it 'can be reached by clicking edit on index page' do
- 			
- 			visit posts_path
+      expect(page).to have_content("Some rationale")
+  	end
 
- 			click_link("edit_#{@post.id}")
- 			expect(page.status_code).to eq(200)
- 		end
- 		it 'can be edited' do
- 			visit edit_post_path(@post)
+    it 'will have a user associated it' do
+      fill_in 'post[date]', with: Date.today
+      fill_in 'post[rationale]', with: "User Association"
+      click_on "Save"
 
- 			fill_in 'post[date]', with: Date.today
-    		fill_in 'post[rationale]', with: "Some rationale"
-      		click_on "Save"
+      expect(User.last.posts.last.rationale).to eq("User Association")
+    end
+  end
 
-      		expect(page.status_code).to eq(200)
- 		end
+  describe 'edit' do
+    before do
+      @post = FactoryGirl.create(:post)
+    end
 
- 	end
+    it 'can be reached by clicking edit on index page' do
+      visit posts_path
+
+      click_link("edit_#{@post.id}")
+      expect(page.status_code).to eq(200)
+    end
+
+    it 'can be edited' do
+      visit edit_post_path(@post)
+
+      fill_in 'post[date]', with: Date.today
+      fill_in 'post[rationale]', with: "Edited content"
+      click_on "Save"
+
+      expect(page).to have_content("Edited content")
+    end
+  end
 end
